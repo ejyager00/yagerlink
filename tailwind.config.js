df7d762a5bf4006a80142260c5d783460c8b4189
Eric Yager <ejyager00@protonmail.com>
2026-02-24 15:58:23 -0500

    Add tailwind to build step, add favicons.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./content/**/*.{html,md}",
+        "./layouts/**/*.html",
+        "./themes/**/layouts/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
